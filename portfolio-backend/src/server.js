a7c9999ba1818913f8129ec183c4968744bb76fe
Alexandre-Ck <alexandre.chikhaoui@laplateforme.io>
2026-06-30 14:53:34 +0200

    config: preparation backend pour vercel

--- portfolio-backend/src/server.js
+++ portfolio-backend/src/server.js
@@ -3,26 +3,33 @@ import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes.js'; 
 import projectRoutes from './routes/project.routes.js';
-import errorHandler from './middlewares/errorHandler.js';// c le middleware des gestion d'erreur 
+import errorHandler from './middlewares/errorHandler.js'; 
 import contactRoutes from './routes/contact.routes.js'; 
-const app = express();  
-const PORT = process.env.PORT || 3001;
 
-// Middlewares globaux
-app.use(cors({ origin: "http://localhost:5173",
+const app = express();  
+
+// Config CORS assouplie pour la production
+app.use(cors({ 
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"] }));
+  allowedHeaders: ["Content-Type", "Authorization"] 
+}));
+
 app.use(express.json());
 
-// Exemple avec une route — à dupliquer pour chaque groupe de routes
+// Tes routes
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/contact', contactRoutes);
-// TODO : brancher les autres routes ici
 
 // Gestionnaire d'erreurs — toujours EN DERNIER
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
-});
+// ❌ On commente le listen pour Vercel
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => {
+//   console.log(`Serveur démarré sur http://localhost:${PORT}`);
+// });
+
+// ✅ On exporte l'application pour Vercel
+export default app;
