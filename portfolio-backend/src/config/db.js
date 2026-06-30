a284534aca721653cde8b1db89af2f7716a0e484
Alexandre-Ck <alexandre.chikhaoui@laplateforme.io>
2026-07-01 00:02:07 +0200

    fix: parse port to int and add ssl for aiven

--- portfolio-backend/src/config/db.js
+++ portfolio-backend/src/config/db.js
@@ -1,13 +1,14 @@
 import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: process.env.DB_PORT || 3306,
+  port: parseInt(process.env.DB_PORT, 10) || 3306, // 100% sécurisé pour Vercel désormais
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false // Requis par Aiven pour accepter les connexions sécurisées en prod
+  }
 });
 
 export default pool;
